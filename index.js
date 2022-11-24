e1d27d1f9571f14312c68679f70ff722e1e29db4
bulat <bulatich1@gmail.com>
2022-11-24 17:51:14 +0300

    create app

--- index.js
+++ index.js
@@ -2,16 +2,32 @@ import axios from 'axios'
 import { config } from 'dotenv'
 import express from 'express'
 
-const PORT = process.env.PORT || 3000
+config()
+const app = express()
+
+const TELEGRAM_URI = `https://api.telegram.org/bot${process.env.TELEGRAM_API_TOKEN}/sendMessage`
 
 app.use(express.json())
-app.use(express.urlencoded({extended: true}));
+app.use(
+        express.urlencoded({
+            extended: true
+        })
+        )
 
-app.post('/start', async (req, res) => {
-    res('HELLO MY FRIEND!')
+app.post('/new-message', async (req, res) => {
+    try {
+  await axios.post(TELEGRAM_URI, {
+    chat_id: chatId,
+    text: 'HELLO WORLD'
+  })
+  res.send('Done')
+} catch (e) {
+  console.log(e)
+  res.send(e)
+    }
 })
 
-
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
