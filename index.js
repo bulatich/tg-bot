5ca51ed05c8c66a4bc191f9034ba1647725a0aae
bulat <bulatich1@gmail.com>
2022-11-24 23:11:01 +0300

    fix

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-// replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.TELEGRAM_API_TOKEN;
 
 // Create a bot that uses 'polling' to fetch new updates
