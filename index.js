c218f0dd0f43d2f5faab055f227a7d85d182b877
bulat <bulatich1@gmail.com>
2022-11-25 15:57:30 +0300

    fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import TelegramBot from 'node-telegram-bot-api';
-import Frontend from './src/frontendPath';
+import Frontend from './src/frontendPath/index.js';
 
 const token = process.env.TELEGRAM_API_TOKEN;
 
